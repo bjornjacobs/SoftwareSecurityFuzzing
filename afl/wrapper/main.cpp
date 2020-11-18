1a87fe385b97f3b34d256ed4cdfa99449fb5d485
Your Name <you@example.com>
2020-11-18 14:29:48 +0100

    wrapper update

--- afl/wrapper/main.cpp
+++ afl/wrapper/main.cpp
@@ -13,22 +13,17 @@ int main(int argc, char** argv)
     std::uint32_t bgColor = 0x00000000;
 
     char* path = nullptr;
-
     path = argv[1];
     fileName = std::string(path);
 
     SVGDocument document;
     if(!document.loadFromFile(fileName)){
-        std::cout << "ERROR COULD not load file"  << std::endl;
+
+        std::cout << "ERROR: COULD not load file"  << std::endl;
         return 1;
     } 
 
-
-    std::string baseName = fileName.substr(fileName.find_last_of("/\\") + 1);
-    baseName.append(".png");
-
-
     Bitmap bitmap = document.renderToBitmap(width, height, 96.0, bgColor);
-    std::cout << "Generated PNG file : "<< baseName << std::endl;
+
     return 0;
 }
