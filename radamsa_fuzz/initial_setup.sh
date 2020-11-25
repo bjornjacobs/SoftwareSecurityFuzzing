fbc07b8a9bdc7709d4704608a6e90a13f6191bf0
MichielVerloop <michiel.verloop@gmail.com>
2020-11-25 16:23:37 +0100

    Add relevant rights to initial_setup.sh

--- radamsa_fuzz/initial_setup.sh
+++ radamsa_fuzz/initial_setup.sh
@@ -1,6 +1,13 @@
+cd fuzz/wrapper
+chmod +x main
+cd ../..
+cd fuzz
+chmod +x run.sh
+mkdir fuzz
+mkdir hangs
+mkdir crashes
+cd ..
 cd radamsa
 sudo make install
 cd ..
-cd wrapper
-chmod +x main
-cd ..
+
